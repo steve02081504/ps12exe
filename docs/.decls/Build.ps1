eb4bc000c91147250eab80d13c18cf4f556d5891
steve02081504 <steve02081504@EDEN.fukwold>
2024-03-23 00:05:11 +0800

    rm decls
    useless

--- docs/.decls/Build.ps1
+++
@@ -1,3 +0,0 @@
-Get-ChildItem $PSScriptRoot -Directory | ForEach-Object {
-	New-ExternalHelp "$PSScriptRoot/$($_.Name)" -OutputPath "$PSScriptRoot/$($_.Name)" -Force
-}
--- docs/.decls/Update.ps1
+++
@@ -1,3 +0,0 @@
-Get-ChildItem $PSScriptRoot -Directory | ForEach-Object {
-	Update-MarkdownHelp "$PSScriptRoot/$($_.Name)" -Force
-}
