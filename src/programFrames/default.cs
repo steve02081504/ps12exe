9b478030698ac551665bd925e20c07a771b1720f
steve02081504 <steve02081504@EDEN.fukwold>
2024-05-19 12:17:45 +0800

    fixes

--- src/programFrames/default.cs
+++ src/programFrames/default.cs
@@ -1,4 +1,4 @@
-// Simple PowerShell host created by Ingo Karstein (http://blog.karstein-consulting.com)
+﻿// Simple PowerShell host created by Ingo Karstein (http://blog.karstein-consulting.com)
 // Reworked and GUI support by Markus Scholtes
 
 using System;
@@ -1791,8 +1791,8 @@ namespace PSRunnerNS {
 			private readonly PSRunnerUI _ui;
 
 			public ConsoleColorProxy(PSRunnerUI ui) {
-                ArgumentNullException.ThrowIfNull(ui);
-                _ui = ui;
+				if (ui == null) throw new ArgumentNullException("ui");
+				_ui = ui;
 			}
 
 			public ConsoleColor ErrorForegroundColor {
