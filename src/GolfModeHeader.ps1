a7e60fdd454080ee0fa737a03194c63dcfb684e1
steve02081504 <steve02081504@EDEN.fukwold>
2025-03-10 01:00:31 +0800

    Update GolfModeHeader.ps1

--- src/GolfModeHeader.ps1
+++ src/GolfModeHeader.ps1
@@ -703,7 +703,7 @@ $SA = "$A"
 $CA = [CHAR[]]$SA
 $NA = $A | ForEach-Object { $_ -as [int] }
 $0 = $1 = $2 = $3 = $4 = $5 = $6 = $7 = $8 = $9 = 0
-$N = $NA[0]
+if ($NA) { $N = $NA[0] }
 
 #endregion
 
